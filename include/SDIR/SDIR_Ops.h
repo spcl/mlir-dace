15551f42ff6f3cbfe199e80b789f4919f7acc2f0
Berke Ates <beates@student.ethz.ch>
2021-06-20 17:02:43 +0200

    simplified

--- include/SDIR/SDIR_Ops.h
+++ include/SDIR/SDIR_Ops.h
@@ -4,7 +4,6 @@
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
-#include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #define GET_OP_CLASSES
 #include "SDIR/SDIR_Ops.h.inc"
