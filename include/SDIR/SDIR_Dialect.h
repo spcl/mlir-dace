781d6cc624e588b877c68d69194cc1159c62c025
Berke Ates <beates@student.ethz.ch>
2021-06-23 13:52:39 +0200

    less imports

--- include/SDIR/SDIR_Dialect.h
+++ include/SDIR/SDIR_Dialect.h
@@ -1,17 +1,15 @@
 #ifndef SDIR_SDIR_DIALECT_H
 #define SDIR_SDIR_DIALECT_H
 
-#include "mlir/IR/Dialect.h"
+#include "mlir/IR/DialectImplementation.h"
 
-#include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/TypeSupport.h"
 #include "mlir/IR/Types.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "mlir/IR/BuiltinTypes.h"
 
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/IR/OpDefinition.h"
-#include "mlir/IR/OpImplementation.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
 
 #include "SDIR/SDIR_OpsDialect.h.inc"
 
