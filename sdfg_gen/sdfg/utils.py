59171ac2986897ee5d1a9beaeedcc7c2f5352954
Berke Ates <beates@student.ethz.ch>
2021-06-23 21:00:37 +0200

    added custom names

--- sdfg_gen/sdfg/utils.py
+++ sdfg_gen/sdfg/utils.py
@@ -1,6 +1,9 @@
-def export_sdfg(sdfg):
-    out_file_json = "gen/json/" + sdfg.name + ".json"
-    out_file_sdfg = "gen/sdfg/" + sdfg.name + ".sdfg"
+def export_sdfg(sdfg, name=None):
+    if name is None: name = sdfg.name
+    else: name = sdfg.name + "_" + name
+
+    out_file_json = "gen/json/" + name + ".json"
+    out_file_sdfg = "gen/sdfg/" + name + ".sdfg"
 
     sdfg.save(filename=out_file_json, use_pickle=False, with_metadata=False, hash=None, exception=None)
     sdfg.save(filename=out_file_sdfg, use_pickle=False, with_metadata=False, hash=None, exception=None)
