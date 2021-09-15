f3df54c769d53fb2fff8360a78e7f42d804b115e
Berke Ates <beates@student.ethz.ch>
2021-09-15 10:00:25 +0200

    removed namespace

--- sdir-opt/sdir-opt.cpp
+++ sdir-opt/sdir-opt.cpp
@@ -7,21 +7,18 @@
 
 #include "SDIR/SDIR_Dialect.h"
 
-using namespace mlir;
-using namespace sdir;
-
 int main(int argc, char **argv) {
-    registerAllPasses();
+    mlir::registerAllPasses();
     // TODO: Register SDIR passes here.
 
-    DialectRegistry registry;
-    registry.insert<SDIRDialect>();
-    registry.insert<StandardOpsDialect>();
+    mlir::DialectRegistry registry;
+    registry.insert<mlir::sdir::SDIRDialect>();
+    registry.insert<mlir::StandardOpsDialect>();
     // Add the following to include *all* MLIR Core dialects, or selectively
     // include what you need like above. You only need to register dialects that
     // will be *parsed* by the tool, not the one generated
     // registerAllDialects(registry);
 
-    return asMainReturnCode(
-                MlirOptMain(argc, argv, "SDIR optimizer driver\n", registry));
+    return mlir::asMainReturnCode(
+                mlir::MlirOptMain(argc, argv, "SDIR optimizer driver\n", registry));
 }
