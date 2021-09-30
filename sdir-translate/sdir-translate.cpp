fc20c583543209533c15e7f4297f233d59e42029
Berke Ates <beates@student.ethz.ch>
2021-09-30 15:24:22 +0200

    added sdir-translate

---
+++ sdir-translate/sdir-translate.cpp
@@ -0,0 +1,13 @@
+#include "mlir/InitAllTranslations.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Translation.h"
+#include "llvm/IR/LLVMContext.h"
+
+#include "SDIR/Translation.h"
+
+int main(int argc, char **argv) {
+  mlir::registerAllTranslations();
+  registerToSDFGTranslation();
+
+  return mlir::failed(mlir::mlirTranslateMain(argc, argv, "MLIR-DaCe Translation Tool"));
+}
