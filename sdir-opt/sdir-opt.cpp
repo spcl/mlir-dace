6f4fb90a9013864bece8568cd0d134456d832482
Berke Ates <beates@student.ethz.ch>
2021-10-24 16:26:33 +0200

    added all dialects to compensate for mlir changes

--- sdir-opt/sdir-opt.cpp
+++ sdir-opt/sdir-opt.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
   // Add the following to include *all* MLIR Core dialects, or selectively
   // include what you need like above. You only need to register dialects that
   // will be *parsed* by the tool, not the one generated
-  // registerAllDialects(registry);
+  registerAllDialects(registry);
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "SDIR optimizer driver\n", registry));
