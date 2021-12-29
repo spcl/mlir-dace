15d6c7fa32b1df1b79bdbff888361431153b4dcc
Berke Ates <beates@student.ethz.ch>
2021-12-29 10:57:27 +0100

    translation test fix

--- test/import_translation_test.py
+++ test/import_translation_test.py
@@ -3,41 +3,50 @@ import json
 import os
 from glob import glob
 from dace import SDFG
-# TODO: Change to LIT tests
-folderPath = os.path.abspath(os.path.dirname(__file__))
+
+# TODO: Add LIT tests
+# TODO: Add this test to CMake tests
+
 
 def check_import(path):
     with open(path) as f:
         if 'XFAIL' in f.read():
             return 0
 
-    result = subprocess.run([folderPath + '/../build/bin/sdir-translate', '--mlir-to-sdfg', path], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    result = subprocess.run(
+        [folderPath + '/../build/bin/sdir-translate', '--mlir-to-sdfg', path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL)
 
-    if(result.returncode != 0):
+    if (result.returncode != 0):
         return 1
-    
-    if(result.stdout.decode('utf-8') == ""):
-        return 0
-    
+
     translated = result.stdout.decode('utf-8')
-    translated_json = json.loads(translated)
+
+    if (translated == ""):
+        return 0
 
     try:
+        translated_json = json.loads(translated)
         sdfg = SDFG.from_json(translated_json)
-        sdfg.save(filename=os.devnull, use_pickle=False, hash=None, exception=None)
+        sdfg.save(filename=os.devnull)
         return 0
     except:
         return 1
 
+
+folderPath = os.path.abspath(os.path.dirname(__file__))
 anyFailed = False
+
 for testPath in glob(folderPath + "/**/*.mlir", recursive=True):
     shortPath = testPath.replace(folderPath, "")
+    # To manually check all the test files
     #print("Testing: " + shortPath)
-    if(check_import(testPath) != 0):
+    if (check_import(testPath) != 0):
         print(shortPath + " failed!")
         anyFailed = True
 
-if(anyFailed):
+if (anyFailed):
     exit(1)
 
 print("All tests passed!")
