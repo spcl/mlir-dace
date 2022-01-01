3cd6a664f3fe1a7ba6ead64f1dc24093e7325951
Berke Ates <beates@student.ethz.ch>
2022-01-01 20:36:59 +0100

    simplified test

--- test/SDIR/Translate/import_translation_test.py
+++ test/SDIR/Translate/import_translation_test.py
@@ -1,17 +1,5 @@
 import json
-import os
+import sys
 from dace import SDFG
 
-
-def check_import(string):
-    try:
-        translated_json = json.loads(string)
-        sdfg = SDFG.from_json(translated_json)
-        sdfg.validate()
-        sdfg.save(filename=os.devnull)
-        return 0
-    except:
-        return 1
-
-
-check_import(os.sys.stdin)
+SDFG.from_json(json.load(sys.stdin)).validate()
