09103eb438ca0e8ef6eafab8c227ecf800945b0d
Berke Ates <beates@student.ethz.ch>
2023-08-03 16:40:50 +0200

    added dialect comments

--- include/SDFG/Dialect/Dialect.h
+++ include/SDFG/Dialect/Dialect.h
@@ -11,9 +11,11 @@
 
 #include "SDFG/Dialect/OpsDialect.h.inc"
 
+/// Generate the code for type definitions.
 #define GET_TYPEDEF_CLASSES
 #include "SDFG/Dialect/OpsTypes.h.inc"
 
+/// Generate the code for operation definitions.
 #define GET_OP_CLASSES
 #include "SDFG/Dialect/Ops.h.inc"
 
